97077621774abc59df5d366000f1de3b91fb658c
Чебанюк Александр <achebanyuk@olko.local>
2019-12-12 16:26:12 +0200

    update invoice Create view

--- resources/views/admin/invoices/create.blade.php
+++ resources/views/admin/invoices/create.blade.php
@@ -3,14 +3,13 @@
 @section('content')
 <style>
     .uper {
-        margin-top: 40px;
+        margin: 40px;
+        /* padding: 50px; */
     }
 </style>
 <div class="card uper">
-    <div class="card-header">
-        Додати рахунок
-    </div>
-    <div class="col-lg-12 well">
+
+    <div class="col-lg-12">
         @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -22,199 +21,129 @@
         @endif
         <form method="post" action="{{ route('invoices.store') }}">
 
-
-            <div class="form-group col-md-6">
-                <label for="company_id">Компанія:</label>
-                <select name="company_id" class="form-control">
-                    <option value="">--- Виберіть компанію ---</option>
-                    @foreach($companies as $id => $company)
-
-                    <option value="{{ $id }}">{{ $company }} </option>
-                    @endforeach
-                </select>
-            </div>
-
-
-
-            <div class="form-row ">
-                <div class="form-group col-md-3">
-                    @csrf
-                    <label for="number">Номер:</label>
-                    <input type="text" class="form-control" name="number" autofocus />
-                </div>
-
-                <div class="form-group col-md-4">
-                    <label for="date">Дата початку періоду:</label>
-                    <input type="date" class="form-control" name="date" />
-                </div>
-                <div class="form-group col-md-4">
-                    <label for="date_end">Дата закінчення періоду:</label>
-                    <input type="date" class="form-control" name="date_end" />
-
-                </div>
-
-            </div>
-            <div class="form-group row">
-                <label for="balance_start" class="col-sm-6 col-form-label font-weight-bold">Сальдо на початок
-                    розрахункового періоду:</label>
-                <div class="col-sm-4"> <input type="text" class="form-control" name="balance_start" />
-                </div>
-                {{-- <div class="dropdown-divider"></div> --}}
-            </div>
-
-
-            <div class="form-group row">
-
-                <label for="tariff_estimated" class="col-sm-6 col-form-label font-weight-bold">Прогнозний тариф на
-                    електричну
-                    енергію:</label>
-
-                <div class="col-sm-4">
-                    <input type="text" class="form-control" name="tariff_estimated" />
-                </div>
-            </div>
-
-            <div class="form-group row">
-                <label for="tariff_transmission" class="col-sm-6 col-form-label "> - тариф на передачу
-                    електроенергії:</label>
-                <div class="col-sm-4"> <input type="text" class="form-control" name="tariff_transmission" /></div>
-            </div>
-
-            <div class="form-group row">
-                <label for="tariff_distribution" class="col-sm-6 col-form-label"> - тариф на розподіл
-                    електроенергії:</label>
-                <div class="col-sm-4">
-                    <input type="text" class="form-control" name="tariff_distribution" />
-                </div>
-            </div>
-
-
-
-
-            <div class="row bg-light text-dark">
-                <div class="col-md-6">
+            <div class="container">
+                <h2 class="uper">Новий рахунок</h2>
+                <div class="col-lg-12 well">
                     <div class="row">
-                        <div class="form-group col-md-12">
-                            <label for="consumption_volume" class="col-form-label">Замовлений обсяг споживання на
-                                розрахунковий період:</label>
-                            <div class="col-sm-8"> <input type="text" class="form-control" name="consumption_volume" />
+                        <form>
+                            <div class="col-sm-12 bg-light text-dark">
+
+                                <div class="form-group">
+                                    <label for="company_id">Компанія:</label>
+                                    <select name="company_id" class="form-control" autofocus>
+                                        <option value="">--- Виберіть компанію ---</option>
+                                        @foreach($companies as $id => $company)
+
+                                        <option value="{{ $id }}">{{ $company }} </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    @csrf
+                                    <label for="number" class="font-weight-bold">Номер рахунку:</label>
+                                    <input type="text" class="form-control col-sm-4" name="number" />
+                                </div>
+
+                                <div class="pt-3">
+                                    <h5>Розрахунковий період</h5>
+                                </div>
+                                <div class="row pb-3">
+
+                                    <div class="col-sm-6 form-group">
+                                        <label for="date">Дата початку:</label>
+                                        <input type="date" class="form-control" name="date" />
+                                    </div>
+                                    <div class="col-sm-6 form-group">
+                                        <label for="date_end">Дата закінчення:</label>
+                                        <input type="date" class="form-control" name="date_end" />
+
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="balance_start" class="font-weight-bold">Сальдо на початок розрахункового
+                                        періоду:</label>
+                                    <input type="text" placeholder="Enter Phone Number Here.."
+                                        class="form-control col-sm-4" name="balance_start">
+                                </div>
+
+
+                                <div class=" pt-3 well">
+                                    <h5>Тарифи на електроенергію</h5>
+                                </div>
+                                <div class="row pb-3">
+                                    <div class="col-sm-4 form-group">
+                                        <label for="tariff_estimated">Прогнозний, у т.ч.:</label>
+                                        <input type="text" class="form-control" name="tariff_estimated" />
+                                    </div>
+                                    <div class="col-sm-4 form-group">
+                                        <label for="tariff_transmission"> - на передачу:</label>
+                                        <input type="text" class="form-control" name="tariff_transmission" />
+                                    </div>
+                                    <div class="col-sm-4 form-group">
+                                        <label for="tariff_distribution"> - на розподіл:</label>
+                                        <input type="text" class="form-control" name="tariff_distribution" />
+                                    </div>
+                                </div>
+
+                                <div class="pt-3">
+                                    <h5>Замовлене споживання електроенергії на розрахунковий період</h5>
+                                </div>
+                                <div class="row pb-3">
+
+                                    <div class="col-sm-6 form-group">
+                                        <label for="consumption_volume">Обсяг споживання:</label>
+                                        <input type="text" class="form-control" name="consumption_volume" />
+                                    </div>
+                                    <div class="col-sm-6 form-group">
+                                        <label for="consumption_cost">Вартість споживання:</label>
+                                        <input type="text" class="form-control" name="consumption_cost" />
+
+                                    </div>
+                                </div>
+
+                                <div class="pt-3">
+                                    <h5>Фактичне споживання електроенергії за розрахунковий період</h5>
+                                </div>
+                                <div class="row pb-3">
+
+                                    <div class="col-sm-6 form-group">
+                                        <label for="consumption_actual">Фактичний обсяг:</label>
+                                        <input type="text" class="form-control" name="consumption_actual" />
+                                    </div>
+                                    <div class="col-sm-6 form-group">
+                                        <label for="cost_actual">Фактична вартість:</label>
+                                        <input type="text" class="form-control" name="cost_actual" />
+
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="paid_summ" class="font-weight-bold">Cплачено в розрахунковому
+                                        періоді:</label>
+                                    <input type="text" class="form-control col-sm-4" name="paid_summ" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="balance_en" class="font-weight-bold">Cальдо на кінець розрахункового
+                                        періоду:</label>
+                                    <input type="text" class="form-control col-sm-4" name="balance_end" />
+                                </div>
+
                             </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-md-12">
-                            <label for="consumption_cost" class="col-form-label">Вартість замовленого споживання на
-                                розрахунковий період:</label>
-                            <div class="col-sm-8"> <input type="text" class="form-control" name="consumption_cost" />
-                            </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
-
-
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="form-group col-md-12">
-                            <label for="consumption_actual" class="col-form-label">Фактичний обсяг споживання:</label>
-                            <div class="col-sm-8"> <input type="text" class="form-control" name="consumption_actual" />
-                            </div>
-                        </div>
+                <div class="col-sm-12 uper">
+                        <button type="submit" class="btn btn-primary">Створити рахунок</button>
                     </div>
-                    <div class="row">
-                        <div class="form-group col-md-12">
-                            <label for="cost_actual" class="col-form-label">Фактична вартість електричної
-                                енергії:</label>
-                            <div class="col-sm-8"> <input type="text" class="form-control" name="cost_actual" /></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="form-group row">
-                <label for="paid_summ" class="col-sm-6 col-form-label font-weight-bold">Сплачено в розрахунковому
-                    періоді:</label>
-                <div class="col-sm-4">
-                    <input type="text" class="form-control" name="paid_summ" />
-                </div>
             </div>
 
 
-            <div class="form-group row">
-                <label for="balance_end" class="col-sm-6 col-form-label font-weight-bold">Сальдо на кінець
-                    розрахункового
-                    періоду:</label>
-                <div class="col-sm-4"> <input type="text" class="form-control" name="balance_end" />
-                </div>
-                <div class="dropdown-divider"></div>
-            </div>
-
-
-            <button type="submit" class="btn btn-primary">Створити рахунок</button>
         </form>
     </div>
+</div>
 
-    <div class="container">
-        <h1 class="well">Новий рахунок</h1>
-        <div class="col-lg-12 well">
-            <div class="row">
-                <form>
-                    <div class="col-sm-12 bg-light text-dark">
-                        <div class="row">
-                            <div class="col-sm-6 form-group">
-                                <label>First Name</label>
-                                <input type="text" placeholder="Enter First Name Here.." class="form-control">
-                            </div>
-                            <div class="col-sm-6 form-group">
-                                <label>Last Name</label>
-                                <input type="text" placeholder="Enter Last Name Here.." class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label>Address</label>
-                            <textarea placeholder="Enter Address Here.." rows="3" class="form-control"></textarea>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-4 form-group">
-                                <label>City</label>
-                                <input type="text" placeholder="Enter City Name Here.." class="form-control">
-                            </div>
-                            <div class="col-sm-4 form-group">
-                                <label>State</label>
-                                <input type="text" placeholder="Enter State Name Here.." class="form-control">
-                            </div>
-                            <div class="col-sm-4 form-group">
-                                <label>Zip</label>
-                                <input type="text" placeholder="Enter Zip Code Here.." class="form-control">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-6 form-group">
-                                <label>Title</label>
-                                <input type="text" placeholder="Enter Designation Here.." class="form-control">
-                            </div>
-                            <div class="col-sm-6 form-group">
-                                <label>Company</label>
-                                <input type="text" placeholder="Enter Company Name Here.." class="form-control">
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label>Phone Number</label>
-                            <input type="text" placeholder="Enter Phone Number Here.." class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Email Address</label>
-                            <input type="text" placeholder="Enter Email Address Here.." class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label>Website</label>
-                            <input type="text" placeholder="Enter Website Name Here.." class="form-control">
-                        </div>
-                        <button type="button" class="btn btn-lg btn-info">Submit</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-
-
-    @endsection
+@endsection
--- resources/views/theme/header.blade.php
+++ resources/views/theme/header.blade.php
@@ -12,7 +12,7 @@
       <div class="container-fluid">
 
         <!-- Page Heading -->
-        <h1 class="h3 mb-4 text-gray-800">Контент</h1>
+        <!-- <h1 class="h3 mb-4 text-gray-800">Контент</h1> -->
         @yield('content')
       </div>
       <!-- /.container-fluid -->
